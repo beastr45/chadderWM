b982d8b0486d2e704ce9927591010a3a09683816
Bear BlinSchauer <bearweston07@gmail.com>
2026-09-01 21:15:34 -0500

    add clagd file, partly solving lsp freaking out, will need to put a full patch later.

--- functions.h
+++ functions.h
@@ -1,33 +1,42 @@
 /* Key binding functions */
-static void defaultgaps(const Arg *arg);
-static void incrgaps(const Arg *arg);
-static void incrigaps(const Arg *arg);
-static void incrogaps(const Arg *arg);
-static void incrohgaps(const Arg *arg);
-static void incrovgaps(const Arg *arg);
-static void incrihgaps(const Arg *arg);
-static void incrivgaps(const Arg *arg);
-static void togglegaps(const Arg *arg);
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+/* These prototypes must match the linkage of the definitions that get pulled in
+ * later (vanitygaps.c, movestack.c, shiftview.c). Those definitions are
+ * non-static, except bstack/bstackhoriz/tile which are static in vanitygaps.c.
+ * Keeping them in sync avoids "static declaration follows non-static" errors
+ * when a tool (clangd) parses this header after those .c files. */
+void defaultgaps(const Arg *arg);
+void incrgaps(const Arg *arg);
+void incrigaps(const Arg *arg);
+void incrogaps(const Arg *arg);
+void incrohgaps(const Arg *arg);
+void incrovgaps(const Arg *arg);
+void incrihgaps(const Arg *arg);
+void incrivgaps(const Arg *arg);
+void togglegaps(const Arg *arg);
 /* Layouts (delete the ones you do not need) */
 static void bstack(Monitor *m);
 static void bstackhoriz(Monitor *m);
-static void centeredmaster(Monitor *m);
-static void centeredfloatingmaster(Monitor *m);
-static void deck(Monitor *m);
-static void dwindle(Monitor *m);
-static void fibonacci(Monitor *m, int s);
-static void gaplessgrid(Monitor *m);
-static void grid(Monitor *m);
-static void horizgrid(Monitor *m);
-static void nrowgrid(Monitor *m);
-static void spiral(Monitor *m);
+void centeredmaster(Monitor *m);
+void centeredfloatingmaster(Monitor *m);
+void deck(Monitor *m);
+void dwindle(Monitor *m);
+void fibonacci(Monitor *m, int s);
+void gaplessgrid(Monitor *m);
+void grid(Monitor *m);
+void horizgrid(Monitor *m);
+void nrowgrid(Monitor *m);
+void spiral(Monitor *m);
 static void tile(Monitor *m);
 /* Internals */
-static void getgaps(Monitor *m, int *oh, int *ov, int *ih, int *iv,
-                    unsigned int *nc);
-static void getfacts(Monitor *m, int msize, int ssize, float *mf, float *sf,
-                     int *mr, int *sr);
-static void setgaps(int oh, int ov, int ih, int iv);
+void getgaps(Monitor *m, int *oh, int *ov, int *ih, int *iv,
+             unsigned int *nc);
+void getfacts(Monitor *m, int msize, int ssize, float *mf, float *sf,
+              int *mr, int *sr);
+void setgaps(int oh, int ov, int ih, int iv);
 
-static void movestack(const Arg *arg);
-static void shiftview(const Arg *arg);
+void movestack(const Arg *arg);
+void shiftview(const Arg *arg);
+
+#endif /* FUNCTIONS_H */
